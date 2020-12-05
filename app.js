a43ae7590b84bf47791c05c429c2b5fe7364c739
jowawash <72688640+jowawash@users.noreply.github.com>
2020-12-05 17:54:28 +0100

    fix typo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.querySelector('.banner button').addEventListener('click', () => {
     document.querySelector('.banner').style.display = 'none';
     document.querySelector('.slideshow-wrapper').style.cssText = 'opacity: 1; visibility: visible';
-    docuemnt.querySelector('.slideshow').style.animation = 'slideshow 24s infinite';
+    document.querySelector('.slideshow').style.animation = 'slideshow 24s infinite';
 });
